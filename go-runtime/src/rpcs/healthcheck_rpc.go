b5638cd21d274a86033b628b11fa62b68b5bc27c
starci183 <cuongnvtse160875@gmail.com>
2024-09-05 00:07:11 +0700

    123123

--- go-runtime/src/rpcs/healthcheck_rpc.go
+++ go-runtime/src/rpcs/healthcheck_rpc.go
@@ -3,22 +3,10 @@ package rpcs
 import (
 	"context"
 	"database/sql"
-	"encoding/json"
 
 	"github.com/heroiclabs/nakama-common/runtime"
 )
 
-type HealthcheckResponse struct {
-	Status bool `json:"status"`
-}
-
 func HealthcheckRpc(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
-	response := &HealthcheckResponse{Status: true}
-
-	out, err := json.Marshal(response)
-	if err != nil {
-		return "", runtime.NewError("Cannot marshal type", 13)
-	}
-
-	return string(out), nil
+	return "ok", nil
 }
