efe38c9898c04be2454fa6603dfa269c8ce842de
starci183 <cuongnvtse160875@gmail.com>
2024-09-05 00:08:20 +0700

    123213

--- go-runtime/src/rpcs/healthcheck_rpc.go
+++ go-runtime/src/rpcs/healthcheck_rpc.go
@@ -3,10 +3,23 @@ package rpcs
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 
 	"github.com/heroiclabs/nakama-common/runtime"
 )
 
+type HealthcheckResponse struct {
+	Status string `json:"status"`
+}
+
 func HealthcheckRpc(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
-	return "ok", nil
+	response := &HealthcheckResponse{Status: "ok"}
+
+	out, err := json.Marshal(response)
+	if err != nil {
+		logger.Error(err.Error())
+		return "", err
+	}
+
+	return string(out), nil
 }
