76feb5b3588426c0166765e150eef9fc2720bfba
starci183 <cuongnvtse160875@gmail.com>
2024-09-04 19:41:49 +0700

    123

--- go-runtime/src/storage/storage.go
+++ go-runtime/src/storage/storage.go
@@ -50,7 +50,6 @@ func InitializeStorage(ctx context.Context, logger runtime.Logger, db *sql.DB, n
 		logger.Error(err.Error())
 		return err
 	}
-	return nil
 
 	err = _config.InitializeConfig(ctx, logger, db, nk, initializer)
 	if err != nil {
