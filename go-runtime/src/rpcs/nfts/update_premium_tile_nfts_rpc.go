08a8d9fe032863ef1d3b8fa1241479145c7bcbb1
starci183 <cuongnvtse160875@gmail.com>
2024-09-13 19:08:43 +0700

    123123

--- go-runtime/src/rpcs/nfts/update_premium_tile_nfts_rpc.go
+++ go-runtime/src/rpcs/nfts/update_premium_tile_nfts_rpc.go
@@ -10,6 +10,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"errors"
+	"sync"
 
 	"github.com/heroiclabs/nakama-common/runtime"
 )
@@ -29,6 +30,8 @@ func UpdatePremiumTileNftsRpc(
 	nk runtime.NakamaModule,
 	payload string,
 ) (string, error) {
+	var wg sync.WaitGroup
+
 	userId, ok := ctx.Value(runtime.RUNTIME_CTX_USER_ID).(string)
 	if !ok {
 		errMsg := "user ID not found"
@@ -49,6 +52,7 @@ func UpdatePremiumTileNftsRpc(
 		return "", err
 	}
 
+	//users nft owned
 	data, err := services_periphery_graphql.GetNftsByOwnerAddress(
 		ctx,
 		logger,
@@ -66,7 +70,7 @@ func UpdatePremiumTileNftsRpc(
 		return "", err
 	}
 
-	//previousNft
+	//previous users nft owned
 	objects, err := collections_inventories.ReadManyByUserId(ctx, logger, db, nk, collections_inventories.ReadManyByUserIdParams{
 		UserId:       userId,
 		ReferenceKey: collections_tiles.KEY_PREMIUM,
@@ -80,10 +84,10 @@ func UpdatePremiumTileNftsRpc(
 		logger.Error(err.Error())
 		return "", err
 	}
-	logger.Info("%v,", len(previousNftInventories))
 
-	//create or transfer from other
 	var tokenIds []int
+
+	//WRITE: create new or received from other
 	for _, nftResponse := range data.Records {
 		object, err := collections_inventories.ReadByTokenId(ctx, logger, db, nk, collections_inventories.ReadByTokenIdParams{
 			TokenId:      nftResponse.TokenId,
@@ -94,7 +98,7 @@ func UpdatePremiumTileNftsRpc(
 			return "", err
 		}
 		if object == nil {
-			//nft not found, mean that you have create new
+			//nft not found in database => create new since new token is minted
 			err := collections_inventories.WriteUnique(ctx, logger, db, nk, collections_inventories.WriteUniqueParams{
 				UserId: userId,
 				Inventory: collections_inventories.Inventory{
@@ -110,7 +114,7 @@ func UpdatePremiumTileNftsRpc(
 				return "", err
 			}
 		} else {
-			//nft found, then we check if previous owner difference from now, so that we procedure a transfer ownership
+			//nft found, mean that it is previously owned by other => do transfer ownership
 			if object.UserId != userId {
 				err := collections_inventories.TransferOwnership(ctx, logger, db, nk, collections_inventories.TransferOwnershipParams{
 					FromUserId: object.UserId,
@@ -125,85 +129,87 @@ func UpdatePremiumTileNftsRpc(
 		}
 		tokenIds = append(tokenIds, nftResponse.TokenId)
 	}
-	//DESTROY or you transfer to others
+	//DELETE: delete or transfer to other
 	for _, previousNftInventory := range previousNftInventories {
-		var found bool
-		for _, nft := range data.Records {
-			if nft.TokenId == previousNftInventory.TokenId {
-				found = true
-				break
-				//do nothing
+		wg.Add(1)
+		go func() error {
+			defer wg.Done()
+			var found bool
+			for _, nft := range data.Records {
+				if nft.TokenId == previousNftInventory.TokenId {
+					found = true
+					break
+				}
 			}
-		}
-		if !found {
-			//not found mean that the previous nft have been disable, there is too case - lose or you transfer
+			if !found {
+				//not found mean previous nfts has the nft current do not, there are 2 case - burned or you transfer to others
 
-			data, err := services_periphery_graphql.GetNftByTokenId(
-				ctx,
-				logger,
-				services_periphery_graphql.GetNftByTokenIdArgs{
-					Input: services_periphery_graphql.GetNftByTokenIdInput{
-						TokenId:  previousNftInventory.TokenId,
-						ChainKey: metadata.ChainKey,
-						Network:  metadata.Network,
-						NftKey:   "premiumTile",
-					},
-				})
-			if err != nil {
-				logger.Error(err.Error())
-				return "", err
-			}
-			if data == nil {
-				//which mean has been burned
-				err := collections_inventories.DeleteUnique(ctx, logger, db, nk, collections_inventories.DeleteUniqueParams{
-					UserId: userId,
-					Key:    previousNftInventory.Key,
-				})
-				if err != nil {
-					logger.Error(err.Error())
-					return "", err
-				}
-			} else {
-				logger.Info(data.OwnerAddress)
-				//the nft still existed, so that has 2 case, the first is the owner inside the application
-				newUserId, err := collections_config.GetUserIdByMetadata(ctx, logger, db, nk, collections_config.GetUserIdByMetadataParams{
-					Metadata: collections_config.Metadata{
-						ChainKey:       metadata.ChainKey,
-						Network:        metadata.Network,
-						AccountAddress: data.OwnerAddress,
-					},
-				})
-				if err != nil {
-					logger.Error(err.Error())
-					return "", err
-				}
-				logger.Info(newUserId)
-				if newUserId != "" {
-					//has, do transfer ownership
-					err := collections_inventories.TransferOwnership(ctx, logger, db, nk, collections_inventories.TransferOwnershipParams{
-						FromUserId: userId,
-						ToUserId:   newUserId,
-						Key:        previousNftInventory.Key,
+				data, err := services_periphery_graphql.GetNftByTokenId(
+					ctx,
+					logger,
+					services_periphery_graphql.GetNftByTokenIdArgs{
+						Input: services_periphery_graphql.GetNftByTokenIdInput{
+							TokenId:  previousNftInventory.TokenId,
+							ChainKey: metadata.ChainKey,
+							Network:  metadata.Network,
+							NftKey:   "premiumTile",
+						},
 					})
-					if err != nil {
-						logger.Error(err.Error())
-						return "", err
-					}
-				} else {
-					logger.Info("called this is correct")
-					//nah, destroy
+				if err != nil {
+					logger.Error(err.Error())
+					return err
+				}
+				if data == nil {
+					//not found, mean it cannot be queried on chain => it have been burned
 					err := collections_inventories.DeleteUnique(ctx, logger, db, nk, collections_inventories.DeleteUniqueParams{
 						UserId: userId,
 						Key:    previousNftInventory.Key,
 					})
 					if err != nil {
 						logger.Error(err.Error())
-						return "", err
+						return err
+					}
+				} else {
+					//the nft is still existed on chain, so that will 2 case
+					newUserId, err := collections_config.GetUserIdByMetadata(ctx, logger, db, nk, collections_config.GetUserIdByMetadataParams{
+						Metadata: collections_config.Metadata{
+							ChainKey:       metadata.ChainKey,
+							Network:        metadata.Network,
+							AccountAddress: data.OwnerAddress,
+						},
+					})
+					if err != nil {
+						logger.Error(err.Error())
+						return err
+					}
+					if newUserId != "" {
+						//case 1, still existed in the database, so that we do transfer ownership
+						err := collections_inventories.TransferOwnership(ctx, logger, db, nk, collections_inventories.TransferOwnershipParams{
+							FromUserId: userId,
+							ToUserId:   newUserId,
+							Key:        previousNftInventory.Key,
+						})
+						if err != nil {
+							logger.Error(err.Error())
+							return err
+						}
+					} else {
+						//nah, mean that it have been transfer to out-of-system address, just delete it
+						err := collections_inventories.DeleteUnique(ctx, logger, db, nk, collections_inventories.DeleteUniqueParams{
+							UserId: userId,
+							Key:    previousNftInventory.Key,
+						})
+						if err != nil {
+							logger.Error(err.Error())
+							return err
+						}
 					}
 				}
 			}
-		}
+			return nil
+		}()
 	}
+	wg.Wait()
 
 	value, err := json.Marshal(UpdatePremiumTileNftsRpcResponse{
 		TokenIds: tokenIds,
